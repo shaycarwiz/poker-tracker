d82b758fc79316ecb26a9a1c972db0f9fbf7b730
Shai <smatsri@gmail.com>
2025-09-20 21:41:30 +0300

    refactor(api): clean up container registration and add TSOA route setup
    - Remove TestController from container registration
    - Add @types/swagger-ui-express dependency
    - Update index.ts with TSOA route registration
    - Clean up import statements and formatting

--- packages/api/src/api/container.ts
+++ packages/api/src/api/container.ts
@@ -2,18 +2,16 @@
 import "reflect-metadata";
 import { diContainer } from "@/infrastructure/di-container";
 import { AuthController } from "./controllers/AuthController";
-
-import { TestController } from "./controllers/TestController";
 import { PlayerController } from "./controllers/PlayerController";
 
 // Register TSOA controllers in the DI container
 diContainer.container.registerSingleton(AuthController, AuthController);
 diContainer.container.registerSingleton(PlayerController, PlayerController);
-diContainer.container.registerSingleton(TestController, TestController);
+
 
 // TSOA container resolver function
 export const iocContainer = {
-  get: <T>(controller: { new (...args: any[]): T }): T => {
+  get: <T>(controller: { new(...args: any[]): T }): T => {
     return diContainer.container.resolve(controller);
   },
 };
